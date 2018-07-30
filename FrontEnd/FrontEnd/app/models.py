8df2eef2c7ae6c15e8a60bfe358b69820c55287c
Shradha Neupane <shradha@vacker360.com>
2018-07-30 21:34:06 +0545

    added database table for dataset and result

--- FrontEnd/FrontEnd/app/models.py
+++ FrontEnd/FrontEnd/app/models.py
@@ -8,3 +8,18 @@ class Prediction(models.Model):
 	algorithm_name = models.CharField(max_length=100)
 	value = models.IntegerField()
 	last_entered = models.DateTimeField()
+
+class data_set(models.Model):
+	date_time = models.DateTimeField()
+	action = models.CharField(max_length = 100)
+	protocol = models.CharField(max_length = 100)
+	src_ip = models.FloatField()
+	dst_ip=models.FloatField()
+	src_port = models.FloatField()
+	dst_port = models.FloatField()
+	path = models.FloatField()
+
+class classified_data(models.Model):
+	category =  models.FloatField()
+	data_set = models.OneToOneField(data_set, on_delete = models.CASCADE)
+
