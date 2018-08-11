56a45b15b45872da67e5cbb73a9567fed48ff44f
Shradha Neupane <shradha@vacker360.com>
2018-08-11 10:30:14 +0545

    preprocessing and prediction in the same function

--- FrontEnd/FrontEnd/app/views.py
+++ FrontEnd/FrontEnd/app/views.py
@@ -39,32 +39,12 @@ def homepage(request):
     knn = Parameters.objects.filter(algorithm_name = "knn").select_related()[0]
     naive = Parameters.objects.filter(algorithm_name = "naive").select_related()[0]
     
-    latest_pred_id = data_set_normalized.objects.all()
-    last_pred_id= len(latest_pred_id)
-    if latest_pred_id == None:
-        last_pred_id = 0
+    
         
     #knnfunc()
     preprocess_log()
     
-    #get the values from database and use prediction
-    cnx = sqlite3.connect("./db.sqlite3")
-    df = pd.read_sql("SELECT * FROM app_data_set_normalized WHERE id > (?)",params=(last_pred_id,), con=cnx)
-    X = np.array(df.drop(["id","data_set_id"],1))
-    y = np.array(len(X))  
-
-    #predict the values
-    y_pred = knn_predict(X)    
-    #save predicted values into a new database
-    print(y_pred)
-    df_pred = pd.DataFrame(y_pred)
-    df_pred["data_set_id"]=df["data_set_id"]
-    df_pred.columns=["category", "data_set_id"]
-    #delete the previous database
-    #predicted_values = classified_data.objects.all()
-    #predicted_values.delete()
-    #add new prediction into the database
-    df_pred.to_sql(name="app_classified_data",if_exists = "append", con =cnx, index=False)
+    
     
 
     #get values to display in database
@@ -158,5 +138,30 @@ def preprocess_log():
     file_add = "C:/Users/Dell/Documents/GitHub/Major-Project/FrontEnd/FrontEnd/app/firewalllog.txt"
     convert_to_csv(file_add, last_id)
     print("converted")
+
+    #predict after preprocessing and store in database
+    latest_pred_id = data_set_normalized.objects.all()
+    last_pred_id= len(latest_pred_id)
+    if latest_pred_id == None:
+        last_pred_id = 0
+    #get the values from database and use prediction
+    cnx = sqlite3.connect("./db.sqlite3")
+    df = pd.read_sql("SELECT * FROM app_data_set_normalized WHERE id > (?)",params=(last_pred_id,), con=cnx)
+    X = np.array(df.drop(["id","data_set_id"],1))
+    y = np.array(len(X))  
+
+    #predict the values
+    y_pred = knn_predict(X)    
+    #save predicted values into a new database
+    print(y_pred)
+    df_pred = pd.DataFrame(y_pred)
+    df_pred["data_set_id"]=df["data_set_id"]
+    df_pred.columns=["category", "data_set_id"]
+    #delete the previous database
+    #predicted_values = classified_data.objects.all()
+    #predicted_values.delete()
+    #add new prediction into the database
+    df_pred.to_sql(name="app_classified_data",if_exists = "append", con =cnx, index=False)
+    print("predicted")
 
 
