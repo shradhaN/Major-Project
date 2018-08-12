17808c7ae75c4719372a074f9de94507148d7956
suravi.vacker360 <suravi@vacker360.com>
2018-08-12 13:16:56 +0545

    added comments to helpers and preprocessin

--- FrontEnd/FrontEnd/app/preprocessin.py
+++ FrontEnd/FrontEnd/app/preprocessin.py
@@ -25,11 +25,11 @@
 #     SEND = 1
 #     '-' = -9999
 
-# Src-port and dst-port
+# Src-port and dst-port were in string initially
 
 # The values were left as it is but convert to neumeric
 
-#     '-' was converted to -99999
+#    '-' was converted to -99999
 
 # Src-ip and dst-ip
 
@@ -40,14 +40,15 @@
 import numpy as np
 import pandas as pd
 import csv
-import socket, struct
-from binascii import hexlify
 
-from sklearn import preprocessing
+import socket, struct # convert ip to num
+from binascii import hexlify # convert ip to num
+
+from sklearn import preprocessing #for normalization
 
 import sqlite3
 from pandas.io import sql
-from app.models import *
+from app.models import * #access to the created db
 from django.db import IntegrityError
 
 
@@ -55,48 +56,30 @@ from django.db import IntegrityError
 
 def ConvertIptoNum(ip):
     try:
+        #IPV4
         #convert decimal dotted quad string to long integer
         return struct.unpack('>L',socket.inet_aton(ip))[0]
     except Exception:
+        #IPV6
         return int(hexlify(socket.inet_pton(socket.AF_INET6, ip)), 16)
 
 def standardize():  
-    print("********************************************")
-    print("chiryo")
-    print("********************************************")
     cnx = sqlite3.connect("./db.sqlite3")      
     #read the csv file        
     df = pd.read_csv('firewall2.csv', dtype=object )
-    #df2 = pd.read_sql("SELECT * FROM app_data_set", con = cnx)
-    
-    #df= df.dropna(axis= "columns", how = "all", inplace=True)
     #drop unnecessary files
     df.drop(['tcpflags','size','tcpsyn','tcpack','tcpwin','icmptype','icmpcode','info'], axis =1, inplace = True)
-    #drop any duplicate files
-    #df.drop_duplicates(keep="first", inplace=True)
     df2=pd.read_sql("SELECT * FROM app_data_set",con=cnx)
     df2.drop(['id'],axis=1, inplace=True)
-    # print("before**************")
-    # print(df.dtypes)
-    # print(df2.dtypes) 
-    
-
-    #print(df)
-    #drop the column if any value is missing
-    #df= df.dropna(axis= "columns", how = "all", inplace=True)
-    #drop unnecessary files
-    #drop any duplicate files
-    #df.drop_duplicates(keep="first", inplace=True)
-
     print("dropped")
-    #print(df2)
-
+    #standarise port
     df['dst_port'] = df['dst_port'].replace('-', -99999)
     df['dst_port'] = df['dst_port'].apply(pd.to_numeric)
     df['src_port'] = df['src_port'].replace('-',-9999)
     df['src_port'] = df['src_port'].apply(pd.to_numeric)
 
     #standardize path
+
     unique_path = df['path'].unique()
     for path in unique_path:
         if path == "RECEIVE":
@@ -106,19 +89,6 @@ def standardize():
         else:
             df["path"]=df['path'].replace(path,-9999)
     df['path'] = df['path'].apply(pd.to_numeric)
-
-    print("middle**************")
-    #print(df.dtypes)
-    #print(df2.dtypes)
-    df2['action'] = df2['action'].apply(pd.to_numeric)
-    df2['protocol'] = df2['protocol'].apply(pd.to_numeric)
-
-    
-    # df_all = df2.merge(df.drop_duplicates(),on=['date','time','action','protocol','src_ip','dst_ip','src_port','dst_port','path'], 
-    #                how='left', indicator=True)
-    # #print(df_all)
-    # df =df_all[df_all._merge != 'both']
-    # print(len(df))
 
 
 
@@ -132,6 +102,7 @@ def standardize():
         else:
             df['action']=df['action'].replace(action,-9999)
     print("action")
+
     #standardize protocol
     unique_protocol = df['protocol'].unique()
     for protocol in unique_protocol:
@@ -146,37 +117,31 @@ def standardize():
     print("protocol")
     
     
-    
+    # replacing - ip with 0.0.0.0
     df['src_ip']=df['src_ip'].replace('-','0.0.0.0')
     df['dst_ip']=df['dst_ip'].replace('-','0.0.0.0')
-    print("protocol done")
-    print("lats")
-    #print(df.dtypes)
-    #print(df2.dtypes)
-    
-        
+
+
+    # convert object type to numeric     
+    df2['action'] = df2['action'].apply(pd.to_numeric)
+    df2['protocol'] = df2['protocol'].apply(pd.to_numeric)
+  
     try:
         df_all = df.merge(df2.drop_duplicates(),on=['date','time','action','protocol','src_ip','dst_ip','src_port','dst_port','path'], 
                        how='left', indicator=True)
-        #print(df_all)
+        #if not in both dfs then unique value is encountered so save in df3
         df3 =df_all[df_all._merge != 'both']
-        print(len(df3))
-        #check and find the risk factors first
-        #use the convert ip to convert the IP to neumeric value
+
         if len(df3) == 0 or len(df3) == 1: 
             pass
-            #df.dropna(inplace=True)
-            #df.to_csv("firewall_standard.csv", index=False)
-            #df.drop(["_merge"], axis=1, inplace=True)
-            #df.to_sql(name="app_data_set", con = cnx, if_exists = "append", index=False)
         else:
             df3.dropna(inplace=True)
-            #print(df3)
-            #df.to_csv("firewall_standard.csv", index=False)
+            #drop the _merge column
             df3.drop(["_merge"], axis=1, inplace=True)
             df3.to_sql(name="app_data_set", con = cnx, if_exists = "append", index=False)
 
     except Exception as e:
+        #for 1st run when all values are unique
         df.dropna(inplace=True)
         df.to_sql(name="app_data_set", con = cnx, if_exists = "append", index=False)
 
@@ -187,18 +152,21 @@ def ip_to_num(latest_id):
     cnx = sqlite3.connect("./db.sqlite3")
     sql_ = """SELECT * FROM app_data_set WHERE id > (?)"""
     df = pd.read_sql(sql_,params = (latest_id,), con=cnx)
-    
-   # print(df)
+
+    #normaizing for source ip
     for ip in df["src_ip"]:
         ip_convert = ConvertIptoNum(ip)
         df["src_ip"]=df["src_ip"].replace(ip,ip_convert)
 
+    #normalize for destination ip
     for ip in df["dst_ip"]:
         ip_convert = ConvertIptoNum(ip)
         df["dst_ip"]=df["dst_ip"].replace(ip,ip_convert)
 
+
     df.drop(['date','time','src_port','dst_port'], axis =1, inplace = True)
-     #normalize the values
+
+    #normalize the values
     min_max_scaler = preprocessing.MinMaxScaler()
     np_scaled = min_max_scaler.fit_transform(df.drop("id", axis=1))
     df2 = pd.DataFrame(np_scaled)
